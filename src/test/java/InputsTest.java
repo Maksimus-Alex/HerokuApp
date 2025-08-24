55a576982a4ecd43cdf6d8fc1effc2f8632bfb02
Maksimus <maxwww76@gmail.com>
2025-08-24 15:51:42 +0300

    HomeWork 20 after remove

--- src/test/java/InputsTest.java
+++ src/test/java/InputsTest.java
@@ -7,12 +7,14 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
 
 public class InputsTest {
 
     WebDriver driver;
+    SoftAssert softAssert;
 
     @BeforeMethod
     public void setup() {
@@ -22,18 +24,20 @@ public class InputsTest {
 
     @Test
     public void ckeckInputsTest() {
+        softAssert = new SoftAssert();
         driver.get("https://the-internet.herokuapp.com/inputs");
         WebElement value = driver.findElement(By.tagName("input"));
         value.click();
         value.sendKeys("Maksimus");
-        Assert.assertEquals(value.getAttribute("value"), "");// принимает в качестве аргумента имя атрибута, значение которого нужно получить
+        softAssert.assertEquals(value.getAttribute("value"), "");// принимает в качестве аргумента имя атрибута, значение которого нужно получить
         value.click();
         value.sendKeys("101");
-        Assert.assertEquals(value.getAttribute("value"), "101");
+        softAssert.assertEquals(value.getAttribute("value"), "101");
         value.sendKeys(Keys.ARROW_UP, Keys.ARROW_UP, Keys.ARROW_UP);// поднимаем значение на 3
-        Assert.assertEquals(value.getAttribute("value"), "104");
+        softAssert.assertEquals(value.getAttribute("value"), "104");
         value.sendKeys(Keys.ARROW_DOWN, Keys.ARROW_DOWN);
-        Assert.assertEquals(value.getAttribute("value"), "102");
+        softAssert.assertEquals(value.getAttribute("value"), "102");
+        softAssert.assertAll();
     }
 
     @AfterMethod(alwaysRun = true)
